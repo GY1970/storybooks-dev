91091ab4181466a940b94eb6e4ca884e8e021f16
GY1970 <gy1970@yahoo.com>
2019-08-12 21:15:43 -1000

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb://glenny:welcome1@ds261817.mlab.com:61817/storybooks-dev',
+  googleClientID:
+    '991646880537-hv9tmqrsqbdnmo1lipfkjb73njojmgo5.apps.googleusercontent.com',
+  googleClientSecret: 'DKMBqoXQ8e76RYecfYBkNC6-'
+};
